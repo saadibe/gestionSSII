9e758d48daa02459738bb3186c461476e6d80d9c
saadibe <saadibechiring@gmail.com>
2017-04-06 18:09:10 +0200

    affichage bug

---
+++ src/main/webapp/js/views/afficherCandidatView.js
@@ -0,0 +1,49 @@
+define([ 'lib/handlebars', 'lib/backbone',
+	'lib/text!templates/afficherCandidat.hbs', 'views/headerView',
+	"models/candidat" ], function(Handlebars, Backbone,
+	template_afficherCandidat, HeaderView, Candidat) {
+    var singleton;
+    var AfficherCandidatView = Backbone.View.extend({
+	tagName : "div",
+	className : "afficherCandidat",
+	events : {
+	    
+	},
+	close : function() {
+	    this.$el.remove();
+
+	    this.off();
+
+	    if (this.model) {
+		this.model.off(null, null, this);
+	    }
+	},
+	render : function() {
+	    var headerView = new HeaderView();
+	    headerView.showMe();
+	    var template = Handlebars.compile(template_afficherCandidat);
+	    this.$el.html(template);
+	    $("#contenu").append(this.$el);
+	    this.delegateEvents();
+	    return this;
+	},
+	showMe : function(candidatId) {
+	    if (!singleton) {
+		singleton = new AfficherCandidatView();
+	    }
+	    candidat = new Candidat({candidatId:candidatId,action:"affichage"});
+	    
+	    candidat.fetch({
+		success : (function(model) {
+		    singleton.model = model
+		    singleton.render();
+		}),
+		error : (function(e) {
+		    console.log(' Service request failure: ' + e);
+		}),
+	    })
+	    return singleton;
+	}
+    });
+    return AfficherCandidatView;
+});
