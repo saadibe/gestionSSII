77e1122788378bcdccf0664dda8e0a4c9cc17167
saadibe <saadibechiring@gmail.com>
2017-05-19 02:36:05 +0200

    css

---
+++ src/main/webapp/js/models/candiadtValidate.js
@@ -0,0 +1,35 @@
+define([ "lib/backbone" ], function(Backbone) {
+    var CandidatValid = Backbone.Model.extend({
+	url : function() {
+	    
+	    switch (this.action) {
+	    case "ajout":
+		return host.url + 'ajoutCandidat';
+		break;
+	    case "affichage":
+		return host.url + 'afficherCandidat/' + this.candidatId;
+		break;
+	    case "supprission":
+		return host.url + 'supprimerCandidat/' + this.candidatId;
+		break;
+	    case "invitation":
+		return host.url + 'afficherCandidatExams/' + this.candidatId;
+		break;
+	    default:
+	      return
+	  }
+	},
+	initialize : function(attributes) {
+	    if (attributes) {
+		this.candidatId = attributes.candidatId;
+		this.action = attributes.action;
+	    }
+	},
+	defaults : {
+	    candidatId : null,
+	    action : null,
+	},
+
+    });
+    return CandidatValid;
+});
